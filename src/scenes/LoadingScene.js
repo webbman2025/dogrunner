ca318b1bf704b5127c925b90a3fd51617372952e
webbman2025 <uiuxdesigner008#gmail.com>
2026-07-21 08:35:15 +0800

    Revert "Replace fake loading delay with real asset download progress."
    This reverts commit b40aab269e3d6636dbe912f38ef97413ad988fde.

--- src/scenes/LoadingScene.js
+++ src/scenes/LoadingScene.js
@@ -24,6 +24,7 @@ const BAR_H = 35;
 const BAR_PAD = 5;
 const BAR_INNER_H = 25;
 const BAR_RADIUS = 20;
+const MOCK_LOAD_MS = 1000;
 
 export default class LoadingScene extends Phaser.Scene {
   constructor() {
@@ -41,14 +42,6 @@ export default class LoadingScene extends Phaser.Scene {
   }
 
   preload() {
-    this.cameras.main.setBackgroundColor(LOADING_BG);
-    this.createProgressBar();
-    this.createProgressLabel();
-
-    this.load.on('progress', (value) => {
-      this.setProgress(value);
-    });
-
     queueGameAssets(this.load, this.textures, { includeRunFrames: true, pet: this.pet });
   }
 
@@ -57,6 +50,8 @@ export default class LoadingScene extends Phaser.Scene {
     hideGameOverMenu();
     this.anims.resumeAll();
     this.tweens.resumeAll();
+
+    this.cameras.main.setBackgroundColor(LOADING_BG);
 
     if (this.anims.exists('loading-run')) {
       this.anims.remove('loading-run');
@@ -70,8 +65,8 @@ export default class LoadingScene extends Phaser.Scene {
     });
 
     this.createPetPreview();
-    this.setProgress(1);
-    this.startGame();
+    this.createProgressBar();
+    this.beginMockLoad();
   }
 
   createPetPreview() {
@@ -124,34 +119,45 @@ export default class LoadingScene extends Phaser.Scene {
     this.setProgress(0);
   }
 
-  createProgressLabel() {
-    this.progressLabel = this.add
-      .text(WIDTH / 2, sy(BAR_Y) + sx(BAR_H) + sy(12), '0%', {
-        fontFamily: 'Arial, sans-serif',
-        fontSize: `${Math.round(sy(16))}px`,
-        color: '#ffffff',
-        stroke: '#000000',
-        strokeThickness: 3,
-      })
-      .setOrigin(0.5, 0)
-      .setDepth(5);
-  }
-
   setProgress(value) {
     const progress = Phaser.Math.Clamp(value, 0, 1);
     const { innerX, innerY, innerW, innerH } = this.barMetrics;
     const fillW = innerW * progress;
 
     this.barFill.clear();
-    if (fillW > 0) {
-      this.barFill.fillStyle(BAR_FILL, 1);
-      this.barFill.fillRect(innerX, innerY, fillW, innerH);
+    if (fillW <= 0) {
+      return;
     }
 
-    this.progressLabel?.setText(`${Math.round(progress * 100)}%`);
+    this.barFill.fillStyle(BAR_FILL, 1);
+    this.barFill.fillRect(innerX, innerY, fillW, innerH);
   }
 
-  startGame() {
+  beginMockLoad() {
+    this.mockLoadComplete = false;
+    this.loadProgress = { value: 0 };
+
+    this.tweens.add({
+      targets: this.loadProgress,
+      value: 1,
+      duration: MOCK_LOAD_MS,
+      ease: 'Linear',
+      onUpdate: () => {
+        this.setProgress(this.loadProgress.value);
+      },
+      onComplete: () => {
+        this.setProgress(1);
+        this.mockLoadComplete = true;
+        this.tryStartGame();
+      },
+    });
+  }
+
+  tryStartGame() {
+    if (!this.mockLoadComplete) {
+      return;
+    }
+
     this.loadingPet?.anims.stop();
     this.scene.start('GameScene', this.pendingGameData);
   }
